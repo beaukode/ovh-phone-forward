5f453ff92c6e979e4d4b9881b88be963433437aa
beaukode <5773637+beaukode@users.noreply.github.com>
2020-01-03 18:56:01 +0100

    Backend: build prod

--- backend/webpack.common.js
+++ backend/webpack.common.js
@@ -1,10 +1,8 @@
-const webpack = require("webpack");
 const path = require("path");
 const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
-  entry: ["webpack/hot/poll?100", "./src/main.ts"],
-  watch: true,
+  entry: ["./src/main.ts"],
   target: "node",
   externals: [
     nodeExternals({
@@ -20,11 +18,9 @@ module.exports = {
       },
     ],
   },
-  mode: "development",
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
   output: {
     path: path.join(__dirname, "dist"),
     filename: "server.js",
---
+++ backend/webpack.dev.js
@@ -0,0 +1,10 @@
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
+const webpack = require("webpack");
+
+module.exports = merge(common, {
+  entry: ["webpack/hot/poll?100"],
+  mode: "development",
+  watch: true,
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+});
